d6a9db567a75f092c9db12b74389a3a5be69d6ff
sanifmujawar <sanifmujawar@gmail.com>
2023-09-01 19:54:47 +0100

    Created Await Prompt With Questions

--- index.mjs
+++ index.mjs
@@ -1,20 +1,105 @@
 // inquirer documentation -  https://www.npmjs.com/package/inquirer
 import inquirer from "inquirer";
-import fs from "node:fs/promises";
+import fs from "fs/promises";
 
-const fs = require("fs");
-const path = require("path");
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+let {
+  title,
+  description,
+  installation,
+  usage,
+  contributing,
+  tests,
+  license,
+  gitHubUsername,
+  email,
+  gitHubRepositoryName,
+} = await inquirer.prompt([
+  {
+    type: "input",
+    name: "title",
+    message: "Project Title:",
+    default() {
+      return "README.md file generator for GitHub";
+    },
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Description of the project:",
+    default() {
+      return "Command-line application to generate README.md files from user input";
+    },
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Installation instructions:",
+    default() {
+      return "Install node.js and clone the reopsitory";
+    },
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Usage information:",
+    default() {
+      return "Run in cli the following command: ```node index.mjs```";
+    },
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Contributing: ",
+    default() {
+      return "Contributions are always welcome!";
+    },
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Tests:",
+    default() {
+      return "To run the test, run the following command: ```npm run test```";
+    },
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "License type:",
+    choices: [
+      "MIT License",
+      "GNU GPL v3",
+      "GNU GPL v2",
+      "BSD 3-Clause",
+      "BSD 2-Clause",
+      "Apache 2.0",
+      "Creative Commons Zero",
+    ],
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Email address for contact:",
+    default() {
+      return "email@example.com";
+    },
+  },
+  {
+    type: "input",
+    name: "gitHubUsername",
+    message: "GitHub username:",
+    default() {
+      return "sanifmujawar";
+    },
+  },
+  {
+    type: "input",
+    name: "gitHubRepositoryName",
+    message: "Repository Name:",
+    default() {
+      return "readme-generator";
+    },
+  },
+]);
 
-// array of questions for user
-const questions = [];
-
-// function to write README file
-function writeToFile(fileName, data) {}
-
-// function to initialize program
-function init() {}
-
-// function call to initialize program
-init();
+// console.log(response);
