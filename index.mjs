5c78c45defba7d9dfe059f24dfd80655ef5b8e4e
sanifmujawar <sanifmujawar@gmail.com>
2023-09-01 19:52:26 +0100

    Imported fs/promises

--- index.mjs
+++ index.mjs
@@ -1,24 +1,20 @@
 // inquirer documentation -  https://www.npmjs.com/package/inquirer
-import inquirer from 'inquirer';
+import inquirer from "inquirer";
+import fs from "node:fs/promises";
 
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
-];
+const questions = [];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
